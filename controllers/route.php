0c43fdb2896abfef652ff964c4159dbacb04ec5c
Eli-Lincon <eli.l.g@hotmail.com>
2017-12-28 23:14:42 -0600

    dinamic menu

---
+++ controllers/route.php
@@ -0,0 +1,20 @@
+<?php
+  class routes{
+
+    function getModulo($url){
+      $url = str_replace('/ShopManager/panel/','',$url);
+      $url = str_replace('.php','',$url);
+      $url = str_replace('.html','',$url);
+      $url = str_replace('/','',$url);
+      return ucwords($url);
+    }
+    function isActive($modulo,$link){
+      if($modulo==$link){
+        return 'class="active-link"';
+      }else{
+        return '';
+      }
+    }
+
+  }
+?>
--- panel/modules/menu.php
+++ panel/modules/menu.php
@@ -1,3 +1,8 @@
+<?php
+  require($_SERVER['DOCUMENT_ROOT'].'/ShopManager/controllers/route.php');
+  $route = new routes();
+  $modulo = $route->getModulo($_SERVER['REQUEST_URI']);
+?>
 <div class="navbar navbar-inverse navbar-fixed-top">
   <div class="adjust-nav">
     <div class="navbar-header">
@@ -18,19 +23,16 @@
 <nav class="navbar-default navbar-side" role="navigation">
   <div class="sidebar-collapse">
     <ul class="nav" id="main-menu">
-      <li >
-          <a href="#" ><i class="fa fa-bars"></i><?php echo ""; ?></a>
-      </li>
-      <li >
+      <li <?php echo $route->isActive($modulo,'');?>>
         <a href="/ShopManager/panel/" ><i class="fa fa-home "></i>Inicio</a>
       </li>
-      <li>
+      <li <?php echo $route->isActive($modulo,'Productos');?>>
         <a href="/ShopManager/panel/productos/"><i class="fa fa-cubes "></i>Productos</a>
       </li>
-      <li>
+      <li <?php echo $route->isActive($modulo,'Examples');?>>
         <a href="/ShopManager/panel/examples.php"><i class="fa fa-table "></i>Examples</a>
       </li>
-      <li class="active-link">
+      <li <?php echo $route->isActive($modulo,'Blank');?>>
         <a href="/ShopManager/panel/blank.php"><i class="fa fa-edit "></i>Blank Page</a>
       </li>
     </ul>
