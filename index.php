780f6194d0562cb875dae0b179944c05367fa3ad
kathdz <lenahmtz@gmail.com>
2017-12-29 01:08:18 -0600

    Login

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
 <html>
   <?php include($_SERVER['DOCUMENT_ROOT'].'/ShopManager/panel/modules/head.php'); ?>
-  <body>
+  <body id="login">
     <div id="wrapper">
-        <div id="page-inner">
-            <div class="col-md-4 col-md-4 col-ossfet">
-              <h2>Login</h2>
-            </div>
-          </div>
-          <hr />
-          <div class="row">
-            <div class="col-md-12">
-              <h3>Contenido aquí</h3>
-            </div>
-          </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-md-offset-4">
+          <h1 class="login">ShopManager</h1>
         </div>
+      </div>
+      <div class="row">
+          <div class="col-lg-4 col-md-4 col-md-offset-4">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h1><i class="fa fa-sign-in" aria-hidden="true"></i> Login</h1>
+              </div>
+              <div class="panel-body">
+                <form class="" action="index.html" method="post">
+                  <div class="input-group">
+                    <span class="input-group-addon">
+                      <i class="fa fa-user" aria-hidden="true"></i>
+                    </span>
+                    <input type="text" class="form-control" placeholder="Usuario">
+                  </div>
+
+                  <div class="input-group">
+                    <span class="input-group-addon">
+                      <i class="fa fa-lock" aria-hidden="true"></i>
+                    </span>
+                    <input type="password" class="form-control" placeholder="Contraseña">
+                  </div>
+                  <a href="#" class="btn btn-primary pull-right">Entrar</a>
+                </form>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   </body>
