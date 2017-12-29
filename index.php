a2e548ae973a9d35ca2a66180886757925ae3dc6
kathdz <lenahmtz@gmail.com>
2017-12-29 00:12:34 -0600

    login

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/ShopManager/panel/modules/head.php'); ?>
+  <body>
+    <div id="wrapper">
+        <div id="page-inner">
+            <div class="col-md-4 col-md-4 col-ossfet">
+              <h2>Login</h2>
+            </div>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Contenido aquí</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
