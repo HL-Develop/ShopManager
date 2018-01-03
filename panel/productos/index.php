e99b9133f8eb76a327340c93cb469e2bd2ffb70b
kathdz <lenahmtz@gmail.com>
2018-01-03 07:38:25 -0600

    Avance Katia

--- panel/productos/index.php
+++ panel/productos/index.php
@@ -11,14 +11,42 @@
       <div id="page-wrapper" >
         <div id="page-inner">
           <div class="row">
-            <div class="col-md-12">
-              <h2>Productos</h2>
+            <div class="col-lg-4 col-md-4">
+                <label>Producto</label>
+                <div class="input-group">
+                  <input name="proveedor" id="proveedor" type="text" required class="form-control">
+                  <span class="input-group-addon">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </span>
+                </div>
             </div>
-          </div>
-          <hr />
-          <div class="row">
-            <div class="col-md-12">
-              <h3>Contenido aquí</h3>
+              <div class="row">
+                <div class="col-lg-12 col-md-6">
+                  <table class="table table-striped table-bordered table-hover">
+                    <caption>Lista de productos</caption>
+                    <thead>
+                      <tr>
+                        <th class="th-center">Stock</th>
+                        <th class="th-center">Codigo</th>
+                        <th class="th-center">Descripcion</th>
+                        <th class="th-center">Precio </th>
+                        <th class="th-center">categoria</th>
+                        <th class="th-center">modificar</th
+                        <th class="th-center">eliminar</th
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td class="td-center">35</td>
+                        <td class="td-center">75004699</td>
+                        <td>Coca Cola 500-ml Vidrio</td>
+                        <td>$ 8.00</td>
+                        <td>Refrescos</td>
+                        <td class="td-center"><i class="fa fa-times text-danger" aria-hidden="true"></i></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
             </div>
           </div>
         </div>
--- panel/ventas/index.php
+++ panel/ventas/index.php
@@ -16,11 +16,69 @@
             </div>
           </div>
           <hr />
+          <!-- formulario de captura -->
           <div class="row">
-            <div class="col-md-12">
-              <h3>Contenido aquí</h3>
+            <div class="col-lg-4 col-md-4">
+                <label>Producto</label>
+                <div class="input-group">
+                  <input name="proveedor" id="proveedor" type="text" required class="form-control">
+                  <span class="input-group-addon">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </span>
+                </div>
+            </div>
+              <div class="col-lg-3 col-md-3 col-md-offset-3">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">
+                    Total Venta
+                  </div>
+                  <div class="panel-body">
+                    <h1>$ 500.00</h1>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <hr/>
+          <!-- Tabla de venta -->
+          <div class="row">
+            <div class="col-lg-12 col-md-6">
+              <table class="table table-striped table-bordered table-hover">
+                <caption>Nota de venta</caption>
+                <thead>
+                  <tr>
+                    <th class="th-center">Cantidad</th>
+                    <th class="th-center">Codigo</th>
+                    <th class="th-center">Descripcion</th>
+                    <th class="th-center">Precio Unitario</th>
+                    <th class="th-center">Total</th>
+                    <th class="th-center">Eliminar</th
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="td-center">1</td>
+                    <td class="td-center">75004699</td>
+                    <td>Coca Cola 500-ml Vidrio</td>
+                    <td>$ 8.00</td>
+                    <td>$ 8.00</td>
+                    <td class="td-center"><i class="fa fa-times text-danger" aria-hidden="true"></i></td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
+          <!--Botón de guardar-->
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-md-offset-8">
+                <div class="form-group">
+                  <label></label>
+                  <button name="save-btn" id="save-btn" type="submit"  class="btn btn-primary btn-lg btn-block pull-right">
+                    <i></i>  Generar venta
+                  </button>
+                </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
